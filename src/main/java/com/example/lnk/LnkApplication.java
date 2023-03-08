ffeffd8030188654d3a623a8ca67fa6aac212619
lnk123 <13057588205@163.com>
2023-03-08 18:54:01 +0800

    Initial commit

---
+++ src/main/java/com/example/lnk/LnkApplication.java
@@ -0,0 +1,13 @@
+package com.example.lnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LnkApplication.class, args);
+    }
+
+}
